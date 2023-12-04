6b990089283183c0b36edc166baa2660f019762b
Ericlys <ericlys_luckas@hotmail.com>
2023-12-04 15:59:00 -0300

    teste

--- Projeto OS/JS/addcliente.js
+++ Projeto OS/JS/addcliente.js
@@ -1,5 +1,16 @@
 
+function handleSubmit(event) {
+    event.preventDefault();
 
+    // formulário para o backend
+    // ...
+    const form = event.target.form;
+    form.reset();
+
+    // Fecha o modal após o envio
+    closeModalAddClientes();
+
+}
 function openModalAddClientes(){
     const ModalAddClientes = document.querySelector('.ModalClientes')
     ModalAddClientes.style.display = 'flex'
@@ -14,12 +25,20 @@ function openModalAddClientes(){
     } else {
         console.error('Botão de fechar não encontrado dentro do modal');
     }
+    const submitButton = ModalAddClientes.querySelector('input[type="submit"]');
+    if (submitButton) {
+        submitButton.addEventListener('click', handleSubmit);
+    } else {
+        console.error('Botão de submit não encontrado dentro do modal');
+    }
+
 }
 
 
 function closeModalAddClientes() {
     const ModalAddClientes = document.querySelector('.ModalClientes');
     if (ModalAddClientes) {
+        
         ModalAddClientes.style.display = 'none';
     } else {
         console.error('Elemento .ModalClientes não encontrado');
