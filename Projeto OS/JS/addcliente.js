bb4bd568b81cb961b89a402efbb82006f7597527
Ericlys <ericlys_luckas@hotmail.com>
2023-12-04 08:04:51 -0300

    Renomeação do index

--- Projeto OS/JS/addcliente.js
+++ Projeto OS/JS/addcliente.js
@@ -1,9 +1,10 @@
 window.onload = function () {
+    
     document.getElementById("contentDiv").addEventListener("click", function(event) {
         const btnAdd = event.target.closest('.BtnAdd');
         if (btnAdd) {
-          
             
+
         }
     });
     document.getElementById("contentDiv").addEventListener("click", function(event) {
